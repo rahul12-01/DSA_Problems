0c4df1d708e7d55f4d4e875eb6d1fea4663f28da
Rahul Soni <134163805+rahul12-01@users.noreply.github.com>
2026-07-14 23:29:52 +0530

    Time: 5 ms (7.79%) | Memory: 46.3 MB (7.94%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,33 @@
+class Solution {
+    // public int freq(char[] chars,char ch){
+    //     int count =0;
+    //     for (int i=0;i<chars.length-1;i++){
+    //         if (chars[i]==chars[i+1])
+    //             count++;
+    //     }
+    //     return count ;
+    // }
+    public int compress(char[] chars) {
+        int count =1;
+        int res=0;
+        char temp =chars[0];
+        for (int i=1;i<chars.length;i++){
+            if (temp!=chars[i]){
+                chars[res++]=temp;
+                temp=chars[i];
+                if (count>1)
+                    for (int k=0;k<(count+"").length();k++)
+                    chars[res++]=(count+"").charAt(k);
+                count=1;
+
+            }
+            else count++;
+        }
+        chars[res++]=temp;
+        if (count>1)
+            for (int k=0;k<(count+"").length();k++)
+            chars[res++]=(count+"").charAt(k);
+        System.out.println(Arrays.toString(chars));
+        return res;
+    }
+}
