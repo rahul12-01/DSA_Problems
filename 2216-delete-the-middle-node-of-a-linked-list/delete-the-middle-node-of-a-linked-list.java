5680155a578a488a4c9aa2f4d5deb4eac7246250
Rahul Soni <134163805+rahul12-01@users.noreply.github.com>
2026-07-27 22:47:21 +0530

    Time: 3 ms (99.96%) | Memory: 202.8 MB (21.46%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -1,33 +1,54 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         ListNode temp = head;
+//         if (head == null || head.next == null)
+//             return null;
+//         int len = 0;
+//         int i=0;
+//         while (temp!=null){
+//             len++;
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         int mid =len/2;
+//         while (temp!=null){
+//             if(i==mid-1)
+//             temp.next=temp.next.next;
+//             else
+//             temp=temp.next;
+//             i++;
+//         }
+//         return head;
+//     }
+// }
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode temp = head;
+
         if (head == null || head.next == null)
             return null;
-        int len = 0;
-        int i=0;
-        while (temp!=null){
-            len++;
-            temp = temp.next;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        int mid =len/2;
-        while (temp!=null){
-            if(i==mid-1)
-            temp.next=temp.next.next;
-            else
-            temp=temp.next;
-            i++;
-        }
+
+        prev.next = slow.next;
+
         return head;
     }
 }
